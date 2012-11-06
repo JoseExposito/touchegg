448f772792fa355989782dffa74d68ea36c12c8e
jose.exposito89 <jose.exposito89@6a4e8e67-9c2c-2891-b3ed-1ffe352c353b>
2012-11-06 21:37:43 +0000

    License changed to GPL2+

--- touchegg/src/touchegg/Main.cpp
+++ touchegg/src/touchegg/Main.cpp
@@ -5,7 +5,7 @@
  *
  * Touchégg is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License  as  published by  the  Free Software
- * Foundation,  either version 3 of the License,  or (at your option)  any later
+ * Foundation,  either version 2 of the License,  or (at your option)  any later
  * version.
  *
  * Touchégg is distributed in the hope that it will be useful,  but  WITHOUT ANY

409536fdadeec5416f4ad7bb77d21120e7d6ef87
JoseExposito <jose.exposito89@gmail.com>
2020-08-16 20:14:15 +0200

    Make clang format work with Ubuntu 18

--- cmake/run-clang-format.sh
+++ cmake/run-clang-format.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Bash script check code style issues with clang-format and show a nice diff
 
-result=$(find src -type f \( -name \*.cpp -o -name \*.h \) -exec sh -c 'clang-format -style=file --Werror {} | diff -u {} -' \;)
+clang-format --help | grep Werror
+if [ -z $? ]; then
+  WerrorFlag="--Werror"
+else
+  WerrorFlag=""
+fi
+
+result=$(find src -type f \( -name \*.cpp -o -name \*.h \) -exec sh -c 'clang-format --style=file $1 "$0" | diff -u "$0" -' {} $WerrorFlag \;)
 
 if [ -z "$result" ]; then
   exit 0

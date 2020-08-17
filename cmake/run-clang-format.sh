7ca0b022487f2ed155fb212fc45521a70252eff0
JoseExposito <jose.exposito89@gmail.com>
2020-08-17 23:10:27 +0200

    Avoid print grep ouutput

--- cmake/run-clang-format.sh
+++ cmake/run-clang-format.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Bash script check code style issues with clang-format and show a nice diff
 
-clang-format --help | grep Werror
-if [ -z $? ]; then
-  WerrorFlag="--Werror"
-else
+hasWerror=$(clang-format --help | grep Werror)
+if [ -z "$hasWerror" ]; then
   WerrorFlag=""
+else
+  WerrorFlag="--Werror"
 fi
 
 result=$(find src -type f \( -name \*.cpp -o -name \*.h \) -exec sh -c 'clang-format --style=file $1 "$0" | diff -u "$0" -' {} $WerrorFlag \;)

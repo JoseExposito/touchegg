4f48b88c77a25827db638b47228ab345f1f0669c
JoseExposito <jose.exposito89@gmail.com>
2020-08-22 13:25:55 +0200

    Add ActionFactory to easily create Action objects

---
+++ src/actions/action-factory.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2011 - 2020 José Expósito <jose.exposito89@gmail.com>
+ *
+ * This file is part of Touchégg.
+ *
+ * Touchégg is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License  as  published by  the  Free Software
+ * Foundation,  either version 2 of the License,  or (at your option)  any later
+ * version.
+ *
+ * Touchégg is distributed in the hope that it will be useful,  but  WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the  GNU General Public License  for more details.
+ *
+ * You should have received a copy of the  GNU General Public License along with
+ * Touchégg. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "actions/action-factory.h"
+
+#include <utility>
+
+#include "actions/action.h"
+#include "actions/maximize-restore-window.h"
+
+std::unique_ptr<Action> ActionFactory::buildAction(
+    ActionType type, std::unordered_map<std::string, std::string> settings) {
+  switch (type) {
+    case ActionType::MAXIMIZE_RESTORE_WINDOW:
+      return std::make_unique<MaximizeRestoreWindow>(std::move(settings));
+
+    default:
+      return nullptr;
+  }
+}
---
+++ src/actions/action-factory.h
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2011 - 2020 José Expósito <jose.exposito89@gmail.com>
+ *
+ * This file is part of Touchégg.
+ *
+ * Touchégg is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License  as  published by  the  Free Software
+ * Foundation,  either version 2 of the License,  or (at your option)  any later
+ * version.
+ *
+ * Touchégg is distributed in the hope that it will be useful,  but  WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the  GNU General Public License  for more details.
+ *
+ * You should have received a copy of the  GNU General Public License along with
+ * Touchégg. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef ACTIONS_ACTION_FACTORY_H_
+#define ACTIONS_ACTION_FACTORY_H_
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "actions/action-type.h"
+#include "actions/action.h"
+
+class ActionFactory {
+ public:
+  static std::unique_ptr<Action> buildAction(
+      ActionType type, std::unordered_map<std::string, std::string> settings);
+};
+
+#endif  // ACTIONS_ACTION_FACTORY_H_

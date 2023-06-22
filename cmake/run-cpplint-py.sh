590ec2f0afde3389f39020b0c9dcba8a2ed07840
Meekness Adesina <zenon8adams@gmail.com>
2023-06-22 01:16:37 +0100

    feat(cpplint): Port cpplint to Python 3
    - Replace Python 2 script with Python 3
    - Update bootstrap script to comply with ShellCheck SC2038

--- cmake/run-cpplint-py.sh
+++ cmake/run-cpplint-py.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script check code style issues with cpplint.py
 
-find src -type f \( -name \*.cpp -o -name \*.h \) | xargs python2 ./cmake/cpplint.py --root src
+find src -type f \( -name \*.cpp -o -name \*.h \) -print0 | xargs -0 python3 ./cmake/cpplint.py --root src
